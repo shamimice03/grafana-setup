0e3cddf1a7c0298c0c5bd4754ab5dbf67d16feea
shamimice03 <shamimice03@gmail.com>
2026-01-01 16:46:54 +0900

    feat: new version

--- docker-envoy-no-ssl/setup.sh
+++ docker-envoy-no-ssl/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
+
 # Create the directory with sudo
 sudo mkdir -p /opt/grafana/data
 
 # Change ownership to the grafana user's UID (472) and GID (472)
 sudo chown -R 472:472 /opt/grafana/data
 
-# docker-compose up -d
+docker-compose up -d
---
+++ docker-envoy-with-ssl/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+
+# Create the directory with sudo
+sudo mkdir -p /opt/grafana/data
+# Change ownership to the grafana user's UID (472) and GID (472)
+sudo chown -R 472:472 /opt/grafana/data
+
+
+# Create the main directory for grafana-proxy
+sudo mkdir /var/lib/grafana-proxy
+
+# Copy everything from the current directory to the new location
+sudo cp -r . /var/lib/grafana-proxy/
+
+# Create necessary subdirectories for Certbot
+sudo mkdir -p /var/lib/grafana-proxy/certbot/conf # Will store the actual certificate files.
+sudo mkdir -p /var/lib/grafana-proxy/certbot/www # Will be used for the Let's Encrypt HTTP challenge.
+
+# Move to the grafana-proxy directory
+cd /var/lib/grafana-proxy/
+
+# Check if docker-compose.yml exists in the target directory
+if [ ! -f docker-compose.yml ]; then
+    echo "Error: docker-compose.yml not found in /var/lib/grafana-proxy/"
+    exit 1
+fi
+
+# check if ./envoy/envoy.yaml exists
+if [ ! -f ./envoy/envoy.yaml ]; then
+    echo "Error: envoy.yaml not found in /var/lib/grafana-proxy/envoy/"
+    exit 1
+fi
+
+# docker-compose up -d
